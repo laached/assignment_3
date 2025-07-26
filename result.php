ddc50c75f5f6ad8826c33792fb071dd880c739f2
laached <147574837+laached@users.noreply.github.com>
2025-07-25 21:00:38 -0700

    Update result.php

--- result.php
+++ result.php
@@ -1,1 +1,31 @@
 
+<?php
+$output = "";
+
+if (isset($_GET['x']) && isset($_GET['y']) && isset($_GET['z'])) {
+    $x = floatval($_GET['x']);
+    $y = floatval($_GET['y']);
+    $z = floatval($_GET['z']);
+
+    $x_escaped = escapeshellarg($x);
+    $y_escaped = escapeshellarg($y);
+    $z_escaped = escapeshellarg($z);
+
+    
+    $command = "python3 process_input.py $x_escaped $y_escaped $z_escaped";
+    $output = shell_exec($command);
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>result</title>
+</head>
+<body>
+    <div>
+        <?php echo $output; ?>
+    </div>
+</body>
+</html>
