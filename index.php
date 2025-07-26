4011f353d939d2c2b842f1a964540ca10d6188fa
laached <147574837+laached@users.noreply.github.com>
2025-07-25 20:57:38 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,29 @@
 
+<!DOCTYPE html>
+<html lang="en">>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Assignment 3 - Input</title>
+</head>
+<body>
+    <h1>Assignment N3</h1>
+
+    <form action="result.php" method="get">
+        <label for="x">Value of x:</label>
+        <input type="number" name="x" id="x" step="any" required><br><br>
+
+        <label for="y">Value of y:</label>
+        <input type="number" name="y" id="y" step="any" required><br><br>
+
+        <label for="z">Value of z:</label>
+        <input type="number" name="z" id="z" step="any" required><br><br>
+
+        <input type="submit" value="Calculate">
+    </form>
+
+    <hr>
+
+    
+</body>
+</html>
